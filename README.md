2083926d309799815bf9763ad5071dcb781424f4
eduarte <efraduarte@gmail.com>
2018-12-01 10:36:13 -0300

    modificar README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # python27
+
+Ejercicios python27
+
+Pasos para subir a Git
+
+# git add . 
+
+# git commit -a 
+
+# git push t
---
+++ ejemplo_ramdom.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding : iso-8859-15
+
+from ramdom import *
+n = int(input("Imgrese la cantidad de lanzar: "))
