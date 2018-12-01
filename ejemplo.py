45d3db4cbf22ce5c8bbb3bdc418620a96d348688
eduarte <efraduarte@gmail.com>
2018-12-01 10:47:39 -0300

    subir archivos py de prueba

---
+++ ejemplo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+numero = int(input('ingrese un numero: '))
+if numero > 0:
+    print ('numero positivo')
+elif numero < 0:
+    print ('numero negativo')
+else:
+    print ('numero es cero')
